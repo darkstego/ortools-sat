c8d6dd9472b4b6c4bb62397e783877bff76f8e64
Abdulla Bubshait <darkstego@gmail.com>
2024-08-12 19:02:35 -0400

    Add tests

--- spec/ortools-sat_spec.cr
+++ spec/ortools-sat_spec.cr
@@ -1,9 +1,70 @@
 require "./spec_helper"
 
-describe Ortools::Sat do
+describe ORTools::Sat do
   # TODO: Write tests
+  #
+  describe ORTools::Sat::Model do
+    model = ORTools::Sat::Model.new
+    bools = [model.new_bool_var, model.new_bool_var, model.new_bool_var]
 
-  it "works" do
-    false.should eq(true)
+    before_each do
+      model = ORTools::Sat::Model.new
+      bools = [model.new_bool_var, model.new_bool_var, model.new_bool_var]
+    end
+
+    describe "#add_bool_or" do
+      it "should make a boolean OR constraint" do
+        model.add_bool_or(bools)
+        solution = model.solve
+        count = bools.count { |bool| solution.true?(bool) }
+        count.should be >= 1
+      end
+    end
+
+    describe "#add_bool_and" do
+      it "should make a boolean AND constraint" do
+        model.add_bool_and(bools)
+        solution = model.solve
+        bools.each { |bool| solution.true?(bool).should be_true}
+      end
+
+      it "should handle negative constraints" do
+        neg_bools = bools.map { |bool| -bool }
+        model.add_bool_and(neg_bools)
+        solution = model.solve
+        bools.each { |bool| solution.true?(bool).should be_false}
+      end
+    end
+
+    describe "#add_bool_xor" do
+      it "should make a boolean XOR constraint" do
+        model.add_bool_xor(bools)
+        solution = model.solve
+        count = bools.count { |bool| solution.true?(bool) }
+        count.odd?.should be_true
+      end
+    end
+
+    describe "#minimize" do
+      it "should minimize the sum of a list of variables" do
+        int_vars = [model.new_int_var(0, 10), model.new_int_var(3, 10), model.new_int_var(5, 10)]
+        model.minimize(int_vars.sum)
+        solution = model.solve
+        solution.solved?.should be_true
+        int_vars.sum { |int_var| solution.value(int_var) }.should eq 8
+      end
+    end
+
+    describe "Linear Constraint" do
+      it "should correctly constrain the solution" do
+        higher = model.new_int_var(0,5)
+        lower = model.new_int_var(4,10)
+        model.add_constraint(higher > lower)
+        solution = model.solve
+        solution.solved?.should be_true
+        solution.value(higher).should eq 5
+        solution.value(lower).should eq 4
+      end
+    end
   end
 end
--- src/ortools-sat.cr
+++ src/ortools-sat.cr
@@ -9,6 +9,9 @@ module ORTools::Sat
 	  fun cp_sat_wrapper_solve(Pointer(UInt8),LibC::SizeT, Pointer(LibC::SizeT)) : Pointer(UInt8)
   end
 
+  # All types that can be converted to a `LinearExpression` with #to_lexpr
+  alias Expressible = (Int32|Int64|IntVar|LinearExpression)
+
   # Methods added to the Int class to make building constraints easier
   struct ::Int
     def to_lexpr : ORTools::Sat::LinearExpression
@@ -28,7 +31,9 @@ module ORTools::Sat
     {% end %}
   end
 
-  # IntVar is the base variable used in the CP-SAT model to find solutions to problems
+  # The base variable used in Constraing Programming (CP) problems.
+  #
+  # This variable should not be instantiated directly, but created with `Model#new_int_var`
   class IntVar
     getter index : Int32
     def initialize(@index)
@@ -50,7 +55,7 @@ module ORTools::Sat
 
     # Macro to create methods for all operators
     {% for op in %w(+ - <= < >= > == !=) %}
-    def {{op.id}}(other : (Int|IntVar|LinearExpression))
+    def {{op.id}}(other : Expressible)
       to_lexpr {{op.id}} other
     end
     {% end %}
@@ -78,12 +83,12 @@ module ORTools::Sat
       LinearExpressionProto.new(vars: @variables, coeffs: @coefficients, offset: @constant)
     end
 
-    def +(other : (Int|IntVar|LinearExpression))
+    def +(other : Expressible)
       other = other.to_lexpr
       LinearExpression.new(@variables + other.variables, @coefficients + other.coefficients, @constant + other.constant)
     end
 
-    def -(other : (Int|IntVar|LinearExpression))
+    def -(other : Expressible)
       other = other.to_lexpr
       LinearExpression.new(@variables + other.variables, @coefficients + other.coefficients.map { |x| -x }, @constant - other.constant)
     end
@@ -92,32 +97,32 @@ module ORTools::Sat
       self
     end
 
-    def <=(other : (Int|IntVar|LinearExpression))
+    def <=(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.less_or_equal(self, other)
     end
 
-    def <(other : (Int|IntVar|LinearExpression))
+    def <(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.less_than(self, other)
     end
 
-    def >=(other : (Int|IntVar|LinearExpression))
+    def >=(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.greater_or_equal(self, other)
     end
 
-    def >(other : (Int|IntVar|LinearExpression))
+    def >(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.greater_than(self, other)
     end
 
-    def ==(other : (Int|IntVar|LinearExpression))
+    def ==(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.equal(self, other)
     end
 
-    def !=(other : (Int|IntVar|LinearExpression))
+    def !=(other : Expressible)
       other = other.to_lexpr
       LinearConstraint.not_equal(self, other)
     end
@@ -175,6 +180,8 @@ module ORTools::Sat
     end
 
     # VARIABLES
+
+    # Provides an IntVar for integer variables
     def new_int_var(min : Int64, max : Int64, name="")
       # Must use not_nil! because the compiler cannot guarantee that the array is not nil
       # even though it is initialized in the constructor
@@ -184,6 +191,7 @@ module ORTools::Sat
       IntVar.new(index)
     end
 
+    # Provides a `BoolVar` for boolean variables
     def new_bool_var(name="")
       variables = @proto.variables.not_nil!
       index = variables.size
@@ -191,34 +199,107 @@ module ORTools::Sat
       BoolVar.new(index)
     end
 
-    def add_all_diff(*args : (LinearExpression|IntVar))
-      vars = args.map { |arg| arg.to_lexpr }.to_a
-      constraint_proto = ConstraintProto.new(all_diff: AllDifferentConstraintProto.new(exprs: vars.map{ |x| x.proto }))
-      @proto.constraints.try &.push(constraint_proto)
+    # CONSTRAINTS
+
+    # Boolean Constraints
+
+    # Forces at least one of the boolean variables to be true
+    #
+    # When *enforcement_literals* are provided, this constraint will only be followed if
+    # all literals are true.
+    def add_bool_or(vars : Array(BoolVar), enforcement_literals=[] of BoolVar)
+      contraint_proto = ConstraintProto.new(bool_or: BoolArgumentProto.new(literals: vars.map{ |x| x.index }),
+                                            enforcement_literal: enforcement_literals.map{ |x| x.index })
+      @proto.constraints.try &.push(contraint_proto)
     end
 
-    def add_exactly_one(*args : (BoolVar))
-      vars = args.to_a
-      add_exactly_one(vars)
+    # Forces all of the boolean variables to be true
+    #
+    # When *enforcement_literals* are provided, this constraint will only be followed if
+    # all literals are true.
+    def add_bool_and(vars : Array(BoolVar), enforcement_literals=[] of BoolVar)
+      contraint_proto = ConstraintProto.new(bool_and: BoolArgumentProto.new(literals: vars.map{ |x| x.index }),
+                                            enforcement_literal: enforcement_literals.map{ |x| x.index })
+      @proto.constraints.try &.push(contraint_proto)
     end
 
+    # Forces an odd number of the provided boolean variables to be true
+    def add_bool_xor(vars : Array(BoolVar))
+      contraint_proto = ConstraintProto.new(bool_xor: BoolArgumentProto.new(literals: vars.map{ |x| x.index }))
+      @proto.constraints.try &.push(contraint_proto)
+    end
+
+    # Forces exactly one of the provided boolean variables to be true
     def add_exactly_one(vars : Array(BoolVar))
       contraint_proto = ConstraintProto.new(exactly_one: BoolArgumentProto.new(literals: vars.map{ |x| x.index }))
       @proto.constraints.try &.push(contraint_proto)
     end
 
-    def add_at_most_one(*args : (BoolVar))
-      vars = args.to_a
-      add_at_most_one(vars)
-    end
-
+    # Forces at most one of the provided boolean variables to be true
     def add_at_most_one(vars : Array(BoolVar))
       contraint_proto = ConstraintProto.new(at_most_one: BoolArgumentProto.new(literals: vars.map{ |x| x.index }))
       @proto.constraints.try &.push(contraint_proto)
     end
 
+    # Linear Constraints
+
+    # Forces the target to equal exprs[0] / exprs[1].
+    # The division is rounded towards zero.
+    # For exact integer division, use product constraint and place the target as an expr.
+    # E.g. a = b * target
+    def add_int_div(target : Expressible, exprs : Array(Expressible))
+      target = target.to_lexpr
+      exprs = exprs.map { |arg| arg.to_lexpr }
+      constraint_proto = ConstraintProto.new(int_div: LinearArgumentProto.new(target: target.proto, exprs: exprs.map{ |x| x.proto }))
+      @proto.constraints.try &.push(constraint_proto)
+    end
+
+    # Forces the target to be equal exprs[0] % exprs[1].
+    def add_int_mod(target : Expressible, exprs : Array(Expressible))
+      target = target.to_lexpr
+      exprs = exprs.map { |arg| arg.to_lexpr }
+      constraint_proto = ConstraintProto.new(int_mod: LinearArgumentProto.new(target: target.proto, exprs: exprs.map{ |x| x.proto }))
+      @proto.constraints.try &.push(constraint_proto)
+    end
+
+    # Forces the target to be equal to the product of the exprs.
+    # The product must fit in an int64 or the model will be invalid.
+    def add_int_prod(target : Expressible, exprs : Array(Expressible))
+      target = target.to_lexpr
+      exprs = exprs.map { |arg| arg.to_lexpr }
+      constraint_proto = ConstraintProto.new(int_prod: LinearArgumentProto.new(target: target.proto, exprs: exprs.map{ |x| x.proto }))
+      @proto.constraints.try &.push(constraint_proto)
+    end
+
+    # Implements a linear constraint in the form of a <= b  or a == b.
+    #
+    # E.g. model.add_constraint( 2*x + 3*y <= 44)
     def add_constraint(constraint : LinearConstraint)
       @proto.constraints.try &.push(constraint.proto)
+    end
+
+    def add_all_diff(vars : Array(Expressible))
+      vars = vars.map { |arg| arg.to_lexpr }
+      constraint_proto = ConstraintProto.new(all_diff: AllDifferentConstraintProto.new(exprs: vars.map{ |x| x.proto }))
+      @proto.constraints.try &.push(constraint_proto)
+    end
+
+    # Creates and equivelence between target variable and the result of the logical OR of the vars
+    def set_to_bool_or(target : BoolVar, vars : Array(BoolVar))
+      add_bool_or(vars, [target])
+      add_bool_and(vars.map { |x| -x }, [-target])
+    end
+
+    # Creates and equivelence between target variable and the result of the logical AND of the vars
+    def set_to_bool_and(target : BoolVar, vars : Array(BoolVar))
+      add_bool_and(vars, [target])
+      add_bool_or(vars.map { |x| -x }, [-target])
+    end
+
+    # Create an objective to minimize
+    def minimize(expr : LinearExpression, domain=[] of Int64, offset : (Float64|Nil) = nil, scaling_factor : (Float64|Nil)=nil)
+      @proto.objective = CpObjectiveProto.new(vars: expr.variables, coeffs: expr.coefficients, offset: offset,
+                                              scaling_factor: scaling_factor, domain: domain)
     end
 
     def solve
@@ -227,8 +308,10 @@ module ORTools::Sat
       return_size_pointer = Pointer(LibC::SizeT).malloc
       result = CPSATWrapper.cp_sat_wrapper_solve(buffer, size, return_size_pointer)
       io = make_io(result, return_size_pointer.value)
-      response = CpSolverResponse.from_protobuf(io)
+      Solution.new CpSolverResponse.from_protobuf(io)
     end
+
+    # Private methods
 
     private def make_buffer(io : IO) : Tuple(Pointer(UInt8), LibC::SizeT)
       bytes = io.to_slice
@@ -243,4 +326,48 @@ module ORTools::Sat
     end
 
   end
+
+  class Solution
+    getter proto : CpSolverResponse
+
+    def initialize(@proto)
+    end
+
+    def status : CpSolverStatus
+      @proto.status
+    end
+
+    def solved? : Bool
+      @proto.status == CpSolverStatus::FEASIBLE || proto.status == CpSolverStatus::OPTIMAL
+    end
+
+    def objective_value : Int64
+      @proto.objective_value
+    end
+
+    def true?(bool_var : BoolVar) : Bool
+      index = bool_var.index
+      value = value(bool_var) == 1
+      index >= 0 ? value : !value
+    end
+
+    def value(int_var : BoolVar) : Int64
+      index = int_var.index
+      value = raw_value index
+      index >= 0 ? value : 1_i64-value
+    end
+
+    def value(int_var : IntVar) : Int64
+      index = int_var.index
+      value = raw_value index
+      index >= 0 ? value : -value
+    end
+
+    private def raw_value(index : Int) : Int64
+      solution = @proto.solution.not_nil!
+      positive_index = index >= 0 ? index : -index - 1
+      solution[positive_index]
+    end
+  end
+
 end
